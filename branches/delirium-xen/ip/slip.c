1a8818a098ccbb5d21ec9a19088a4fb68fcc534e
shigawire <shigawire@5a767eba-9347-0410-ade0-d5b39c178e61>
2008-06-27 17:56:27 +0000

    add some more code to the SLIP driver

--- branches/delirium-xen/ip/slip.c
+++ branches/delirium-xen/ip/slip.c
@@ -6,6 +6,14 @@
 #include "delibrium/delibrium.h"
 #include "slip.h"
 
+#define SLIPDEBUG
+
+/*
+ * SLIP driver
+ *
+ * TODO: Move the serial thread stuff out of here
+ */
+
 soapbox_id_t	ipv4_soapbox = 0;
 u_int16_t	serial_base;
 
@@ -13,20 +21,53 @@ void ipv4_listener(message_t msg) {
 }
 
 void serial_listener(message_t msg) {
+	#ifdef SLIPDEBUG
+	printf("[slip] Got 0x%2x from serial port\n", msg.m.signal);
+	#endif
+}
+
+void slip_serial_rx_thread() {
+	// This be serial thread.
+	soapbox_id_t serial_sb;
+	
+	// TODO: stop hardcoding the serial port number
+	if (!(serial_sb = get_new_soapbox("/hardware/serial/0"))) {
+		print("slip_serial_rx_thread: /hardware/serial/0 already registered");
+		return;
+	}
+
+	for (;;) {
+		char inchar;
+		message_t msg;
+
+		inchar = read_serial(serial_base);
+		
+		msg.type = signal;
+		msg.m.signal = inchar;
+		rant(serial_sb, msg);
+	}
 }
 
 void slip_start_interface() {
 	soapbox_id_t default_sb;
+	soapbox_id_t serial_input_sb;
 
+	if (! (serial_input_sb = get_soapbox_from_name("/hardware/serial/0"))) {
+		print("slip_start_interface: couldn't find soapbox for /hardware/serial/0");
+		return;
+	}
 	
 	if (!(ipv4_soapbox = get_new_soapbox("/network/interface/slip/ip"))) {
-		print("slip_start_interface: interface already registered");
+		print("slip_start_interface: /network/interface/slip/ip already registered");
 		return;
 	}
 	
 
-	if (supplicate(ipv4_soapbox, ipv4_listener)) {
-		print("slip_start_interface: error registering interface");
+	if (supplicate(ipv4_soapbox, ipv4_listener) == 0) {
+		print("slip_start_interface: error registering ipv4_listener");
+	}
+	if (supplicate(serial_input_sb, serial_listener) == 0) {
+		print("slip_start_interface: error registering serial_listener");
 	}
 	
 	// Attempt to register as the default_sb IP provider, but only if there
@@ -34,7 +75,7 @@ void slip_start_interface() {
 	// network stack, we don't interfere with it, but lightweight
 	// implementations still get the benefit of a default_sb route without
 	// the router
-	default_sb = ipv4_soapbox = get_new_soapbox("/network/ip/route/default_sb");
+	default_sb = get_new_soapbox("/network/ip/route/default_sb");
 	if (default_sb)
 		supplicate(default_sb, ipv4_listener);
 }
@@ -63,5 +104,6 @@ inline void slip_send(char * packet, size_t len) {
 void slip_init(u_int16_t base_port, size_t speed) {
 	serial_base = base_port;
 	init_serial(base_port, speed);
+	new_thread(slip_serial_rx_thread);
 	new_thread(slip_start_interface);
 }
