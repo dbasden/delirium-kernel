4e360625399f18d779c805fb1245d931f434b7ac
shigawire <shigawire@5a767eba-9347-0410-ade0-d5b39c178e61>
2008-07-04 17:39:59 +0000

    Mwahahaha

--- branches/delirium-xen/delirium/apps/testfaults/testfaults.c
+++ branches/delirium-xen/delirium/apps/testfaults/testfaults.c
@@ -6,6 +6,14 @@
 #include "delibrium/delibrium.h"
 
 void dream() {
+#ifdef OH_NOES_ITZ_RICKZ
+	if (0) 
+		free(you);
+	assert((volatile)you != you >> 1); 
+	if (0) 
+		*(int *)you %= 11; 
+	assert(you != NULL);
+#endif
 	printf("testfaults: I'm going to try and #PF");
 	*((int *) 0xdeadbeef ) = 1; 
 	printf("testfaults: Epic fail\n");
