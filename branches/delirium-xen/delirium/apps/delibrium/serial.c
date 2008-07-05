467f812341a164b03f0b41d89f75a04ecbc14a0d
shigawire <shigawire@5a767eba-9347-0410-ade0-d5b39c178e61>
2008-07-05 11:52:45 +0000

    Pulled the C hardware interrupt handler wrapper ISRs into userspace, along with the ability to remove a hardware interrupt handler from userspace.
    Cleaned up the userspace 16450/16550 serial code in delibrium quite a bit:
    
    - The evil blocking calls that will spin on inb should be avoided, especially
      for blocked reads. Names have been changed deliberately to state they
      are blocking and commented appropriately
    
    - delibrium/serial.h now has macros to check the IO ports directly to 
      see if data is waiting, and init_serial will now setup the UART to 
      trigger an interrupt when a byte comes in.  
    
    - added consume_serial_buffer to read and discard bytes from the UART
      buffer until there are no bytes ready
    
    Current practice for userspace code wanting to talk to a serial port is
    to:
    
    	- register a new soapbox "/hardware/serial/0" (where 0 is the
    	  first serial port). If this already taken, assume the UART is
    	  already in use by something else
    	- attach a C interrupt handler to the IRQ for the UART you
    	  are using. The interrupt handler should do as little as
    	  possible (and should avoid if at all possible calling
    	  any library functions, ESPECIALLY in userspace unless
    	  explicitly marked safe to do so), make sure that there
    	  is actually data waiting still and never, ever block. See
    	  apps/slip/slip.c for an example.
    	- Make sure that there is space in the UART before transmitting.

--- branches/delirium-xen/delirium/apps/delibrium/serial.c
+++ branches/delirium-xen/delirium/apps/delibrium/serial.c
@@ -1,6 +1,13 @@
 #include <delirium.h>
 #include <i386/io.h>
+#include "serial.h"
 
+#define SERIAL_IRQ_EVERY_1_WORD		0x00
+#define SERIAL_IRQ_EVERY_4_WORDS	0x40
+#define SERIAL_IRQ_EVERY_8_WORDS	0x80
+#define SERIAL_IRQ_EVERY_14_WORDS	0xc0
+
+#define SERIAL_IRQ_RX_DATA_AVAIL	0x01
 /*
  * Setup a 16[45]50 UART
  *
@@ -13,24 +20,33 @@ void init_serial(u_int16_t baseport, size_t speed) {
 	divisor = 115200 / speed;
 
 	outb(baseport + 1, 0);		/* Disable interrupts */
+	outb(baseport + 1, SERIAL_IRQ_RX_DATA_AVAIL);	
 	outb(baseport + 3, 0x80);	/* Divisor Latch Enable */
 	outb(baseport, (divisor % 0xff));
 	outb(baseport + 1, ((divisor >> 8) % 0xff));
 	outb(baseport + 3, 0x03);	/* 8N1 */
-	outb(baseport + 2, 0xc7);	/* Use FIFOs. Clear, and set IRQ trigger to 14 */
+	outb(baseport + 2, 0x07 | SERIAL_IRQ_EVERY_1_WORD);	/* Use FIFOs. Clear, and set IRQ trigger to every word */
 	outb(baseport + 4, 0x03);	/* DTS/RTS */
 	outb(baseport + 1, 0x01);	/* Enable Interrupts */
 }
 
-inline char read_serial(u_int16_t baseport) {
+/* consume and discard buffer */
+inline void consume_serial_buffer(u_int16_t baseport)  {
+	u_int8_t c;
+	while (( (c = inb(baseport + 5)) & 1 ) != 0)
+		c = inb(baseport);
+}
+
+/* Don't use this. Really. */
+inline char blocking_read_serial(u_int16_t baseport) {
 	char c;
 
-	do { yield(); c = inb(baseport + 5); } while ((c & 1) == 0);
+	do { c = inb(baseport + 5); } while ((c & 1) == 0);
 	c = inb(baseport);
 	return c;
 }
 
-inline void send_serial(u_int16_t baseport, char ch) {
+inline void blocking_send_serial(u_int16_t baseport, char ch) {
 	char c;
 
 	do { c = inb(baseport + 5); } while ((c & 0x20) == 0);
